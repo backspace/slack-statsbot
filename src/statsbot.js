0a122405328420d585fcb2589b818764ad1df438
Buck Doyle <b@chromatin.ca>
2015-01-21 15:21:06 -0500

    Log in using Slack client

--- src/statsbot.js
+++ src/statsbot.js
@@ -3,6 +3,7 @@ var SlackClient = require('slack-client');
 class StatsBot {
   constructor() {
     this.client = new SlackClient(process.env.SLACK_TOKEN);
+    this.client.login();
   }
 }
 
--- test/statsbot.js
+++ test/statsbot.js
@@ -3,19 +3,25 @@ var test = require('tape');
 var sinon = require('sinon');
 var requireSubvert = require('require-subvert')(__dirname);
 
-test('Constructing a StatsBot starts a SlackClient', function(t) {
-  t.plan(2);
+test('Constructing a StatsBot starts a SlackClient and logs in', function(t) {
+  t.plan(3);
 
   var sandbox = sinon.sandbox.create().stub(process, 'env', {'SLACK_TOKEN': 'atoken'});
 
-  var stub = sinon.stub();
+  var client = {login: function() {}};
+  var stub = sinon.stub().returns(client);
   requireSubvert.subvert('slack-client', stub);
+
+  var mock = sinon.mock(client);
+  mock.expects('login');
 
   var StatsBot = requireSubvert.require('../src/statsbot');
   var bot = new StatsBot();
 
   t.ok(stub.calledOnce, 'should call SlackClient constructor once');
   t.ok(stub.calledWith('atoken'), 'should construct SlackClient with environment variable token');
+
+  t.ok(mock.verify(), 'should log in');
 
   sandbox.restore();
 });

743b4d4610f31ff84ed6a26bd41437b5f348e556
Aria Stewart <aredridel@dinhe.net>
2015-10-26 19:50:47 -0400

    Prioritize race above gender since we get it more wrong, also stop relying on filesystem directory read order

--- src/attribute-configurations.js
+++ src/attribute-configurations.js
@@ -4,6 +4,7 @@ var map = require('lodash.map');
 var path = require('path');
 var attributesPath = path.resolve(__dirname, '../attributes/');
 
-module.exports = map(fs.readdirSync(attributesPath), function(filename) {
-  return require(`../attributes/${filename}`);
-});
+module.exports = [
+    require('../attributes/pocness.json'),
+    require('../attributes/manness.json')
+];

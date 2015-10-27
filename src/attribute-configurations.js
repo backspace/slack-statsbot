45a07187f83a0a1a852e1f86971074140e648577
Buck Doyle <b@chromatin.ca>
2015-10-27 13:38:15 -0400

    Remove unused imports

--- src/attribute-configurations.js
+++ src/attribute-configurations.js
@@ -1,9 +1,3 @@
-var fs = require('fs');
-var map = require('lodash.map');
-
-var path = require('path');
-var attributesPath = path.resolve(__dirname, '../attributes/');
-
 module.exports = [
   require('../attributes/pocness.json'),
   require('../attributes/manness.json')

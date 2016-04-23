0eba7433bfa84c6f537f5d52051e90b904d2e413
Buck Doyle <b@chromatin.ca>
2016-04-23 12:13:20 +0200

    Remove unknownValue definitions
    These are now defined the same was as known values.

--- src/attribute-configurations.js
+++ src/attribute-configurations.js
@@ -1,4 +1,23 @@
+const find = require('lodash.find');
+
+/**
+ * The attribute configurations used to contain an unknownValue property that
+ * contained the texts for when an attribute value was unknown. Now that
+ * people can set their attribute value to unknown, the value definition
+ * is no different than the various known values. This restores the unknownValue
+ * property until the code that depends on it can be removed.
+ */
+
+function deriveUnknownValue(attributeConfiguration) {
+  const unknownValue = find(attributeConfiguration.values, valueConfiguration => valueConfiguration.value === null);
+  attributeConfiguration.unknownValue = {
+    texts: unknownValue.texts
+  };
+
+  return attributeConfiguration;
+}
+
 module.exports = [
-  require('../attributes/pocness.json'),
-  require('../attributes/manness.json')
+  deriveUnknownValue(require('../attributes/pocness.json')),
+  deriveUnknownValue(require('../attributes/manness.json'))
 ];

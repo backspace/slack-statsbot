96af731e7b91d4db09b5bde88928f12b51ccbdf2
Buck Doyle <b@chromatin.ca>
2016-07-09 17:46:50 -0500

    Add handling of more information response

--- src/message-buttons/server.js
+++ src/message-buttons/server.js
@@ -1,12 +1,23 @@
 const koa = require('koa');
 const Router = require('koa-router');
 
+const bodyParser = require('koa-body');
+
 module.exports = function() {
   const app = koa();
+  app.use(bodyParser());
+
   const router = new Router();
 
   router.post('/slack/actions', function* (next) {
-    this.body = 'Aww!';
+    const payload = JSON.parse(this.request.body.payload);
+    const action = payload.actions[0];
+
+    if (action.value === 'more') {
+      this.body = 'Here is more information.';
+    } else {
+      this.body = 'Aww!';
+    }
 
     yield next;
   });
--- test/message-buttons/server.js
+++ test/message-buttons/server.js
@@ -4,6 +4,20 @@ const agent = require('supertest-koa-agent');
 const test = require('tape');
 const sinon = require('sinon');
 
+test('it handles a more information request from the initial interview question', function(t) {
+  agent(startServer())
+    .post('/slack/actions')
+    .type('form')
+    .send({payload: JSON.stringify({
+      callback_id: 'initial',
+      actions: [{
+        name: 'more',
+        value: 'more'
+      }]
+    })})
+    .expect(200, 'Here is more information.', t.end);
+});
+
 test('it handles a rejection of the initial interview question', function(t) {
   agent(startServer())
     .post('/slack/actions')

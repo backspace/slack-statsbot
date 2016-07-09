80a871084221be5a43f6af26fbd347b3358ecc7b
Buck Doyle <b@chromatin.ca>
2016-07-09 18:21:11 -0500

    Correct imports

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
-const attributeConfigurations = require('./attribute-configurations');
-const questionForAttributeConfiguration = require('src/message-buttons/question-for-attribute-configuration');
+const attributeConfigurations = require('./src/attribute-configurations');
+const questionForAttributeConfiguration = require('./src/message-buttons/question-for-attribute-configuration');
 
-const server = require('./src/message-buttons/server')({attributeConfiguration, questionForAttributeConfiguration});
+const server = require('./src/message-buttons/server')({attributeConfigurations, questionForAttributeConfiguration});
 
 server.listen(process.env.PORT || 3000);
